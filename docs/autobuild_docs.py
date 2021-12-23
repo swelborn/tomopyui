b350a24133cee2a2244f0fe5eeb2b4c4ba00a578
samwelborn <swelborn@seas.upenn.edu>
2021-12-22 22:31:05 -0800

    docs

--- docs/autobuild_docs.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from livereload import Server, shell
-server = Server()
-server.watch('../tomopyui/widgets/main.py', shell('sphinx-build . _build -b html'))
-
-server.serve(root='_build/html')
